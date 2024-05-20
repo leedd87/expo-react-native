1c8ca14ebe0b6681759ae309212a0fad4122c84f
leedd87 <leedav87@gmail.com>
2024-05-20 11:47:07 -0300

    add first image

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,32 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 export default function Index() {
+  const PlaceholderImage = require('../assets/images/background-image.png');
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#25292e',
-      }}
-    >
-      <Text style={{ color: '#fff' }}>Hola</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
