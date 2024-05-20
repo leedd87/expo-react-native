13435faf7a9eca1b993f6f71405b9acdf1f25f77
leedd87 <leedav87@gmail.com>
2024-05-20 11:43:01 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#25292e',
+      }}
+    >
+      <Text style={{ color: '#fff' }}>Hola</Text>
+    </View>
+  );
+}
